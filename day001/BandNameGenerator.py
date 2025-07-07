3ee3516ba44d7f5950167f74816b483151453b20
Abdalfatah Hoorani <aboodhourani123321@gmail.com>
2025-07-08 00:43:07 +0200

    first try for Rock Paper and Scissors game

---
+++ day001/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Hello! This is a band name generator!")
+city_name = input("What's the name of the city you grew up in? ")
+pet_name = input("What's your pet name? ")
+print("your band name could be " + city_name + " " + pet_name)
